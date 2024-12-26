826475ccc1d51917426d83e85845158a5d3ad868
katastraa <madeleine2144@gmail.com>
2024-12-26 10:57:38 +0100

    cosas yo que se ha este punto estoy harta

---
+++ Assets/Scripts/CasaDelaPili.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CasaDelaPili : MonoBehaviour
+{
+    public Button Madre;
+    public Button MadreCocina;
+    public Button DialogoDerecha;
+    public Button DialogoAbajo;
+    public Button NavegacionDerecha;
+    public Button NavegacionIzquierda;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void madreDialogo ()
+    {
+
+    }
+
+    public void madreCocina () 
+    { 
+    }
+}
